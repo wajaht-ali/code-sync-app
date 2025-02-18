69c3d0e585bfbaddd3f6511e8bf829a701241d38
wajaht-ali <contact.chwajahat@gmail.com>
2025-02-18 10:01:00 +0500

    project initialization

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles/App.css";
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-blue-600'>Vite + React</h1>
+      
+      <p className="text-red-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
